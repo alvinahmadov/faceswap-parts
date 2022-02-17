793ba66058f286cd07cc1dd930ac14e4a8278456
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-02-17 10:41:18 +0400

    Update imports Refactor block names Rename params

--- networks/nn_blocks.py
+++ networks/nn_blocks.py
@@ -1,36 +1,40 @@
+import keras.backend as K
 import tensorflow as tf
-import tensorflow.python.keras.backend as K
-from keras.layers import BatchNormalization
-from tensorflow.python.keras.layers import (
-    Conv2D, Reshape, Lambda, Permute,
-    Softmax, Activation, UpSampling2D,
+from keras import regularizers
+from keras.layers import (
+    Conv2D, Reshape, Lambda, Permute, LeakyReLU,
+    Softmax, Activation, UpSampling2D, BatchNormalization,
     add, concatenate, multiply
 )
-from tensorflow.python.keras.layers.advanced_activations import LeakyReLU
-from tensorflow.python.keras import regularizers
 
 from .custom_inits.icnr_initializer import icnr_keras
-from .custom_layers.scale_layer import Scale
 from .group_normalization import GroupNormalization
 from .instance_normalization import InstanceNormalization
+from .layers.scale_layer import Scale
 from .pixel_shuffler import PixelShuffler
 
 KERN_INIT = 'he_normal'
 W_L2 = 1e-4
 
 
-def self_attn_block(inp, nc, squeeze_factor=8):
+def sagan_block(inputs, num_input_channels, squeeze_factor=8):
     """
+    SAGAN block
+
+    References
+    ------
+    Self-Attention Generative Adversarial Network (SAGAN) (https://arxiv.org/pdf/1805.08318.pdf)
+
     Code borrows from https://github.com/taki0112/Self-Attention-GAN-Tensorflow
     """
-    assert nc // squeeze_factor > 0, \
-        f"Input channels must be >= {squeeze_factor}, got nc={nc}"
-    x = inp
-    shape_x = x.get_shape().as_list()
 
-    f = Conv2D(nc // squeeze_factor, 1, kernel_regularizer=regularizers.l2(W_L2))(x)
-    g = Conv2D(nc // squeeze_factor, 1, kernel_regularizer=regularizers.l2(W_L2))(x)
-    h = Conv2D(nc, 1, kernel_regularizer=regularizers.l2(W_L2))(x)
+    assert num_input_channels // squeeze_factor > 0, \
+        f"Input channels must be >= {squeeze_factor}, got num_input_channels={num_input_channels}"
+    shape = inputs.get_shape().as_list()
+
+    f = Conv2D(num_input_channels // squeeze_factor, 1, kernel_regularizer=regularizers.l2(W_L2))(inputs)
+    g = Conv2D(num_input_channels // squeeze_factor, 1, kernel_regularizer=regularizers.l2(W_L2))(inputs)
+    h = Conv2D(num_input_channels, 1, kernel_regularizer=regularizers.l2(W_L2))(inputs)
 
     shape_f = f.get_shape().as_list()
     shape_g = g.get_shape().as_list()
@@ -43,69 +47,78 @@ def self_attn_block(inp, nc, squeeze_factor=8):
 
     beta = Softmax(axis=-1)(s)
     o = Lambda(lambda x_: K.batch_dot(x_[0], x_[1]))([beta, flat_h])
-    o = Reshape(shape_x[1:])(o)
+    o = Reshape(shape[1:])(o)
     o = Scale()(o)
 
-    out = add([o, inp])
-    return out
+    return add([o, inputs])
 
 
-# TODO: Set norm type adequate
-def dual_attn_block(inp, nc, squeeze_factor=8, norm='none'):
-    assert nc // squeeze_factor > 0, \
-        f"Input channels must be >= {squeeze_factor}, got nc={nc}"
-    x = inp
-    shape_x = x.get_shape().as_list()
+def dan_block(inputs, num_input_channels, squeeze_factor=8, norm='none'):
+    """
+    Dual Attention Network block
+
+    References
+    ------
+    Dual Attention Network for Scene Segmentation (https://arxiv.org/pdf/1809.02983.pdf)
+
+    Code from https://github.com/junfu1115/DANet
+    """
+    assert num_input_channels // squeeze_factor > 0, \
+        f"Input channels must be >= {squeeze_factor}, got num_input_channels={num_input_channels}"
+    x = inputs
+    shape = inputs.get_shape().as_list()
 
     # position attention module
-    x_pam = Conv2D(nc, kernel_size=3, kernel_regularizer=regularizers.l2(W_L2),
-                   kernel_initializer=KERN_INIT, use_bias=False, padding="same")(x)
+    x_pam = Conv2D(num_input_channels, kernel_size=3, use_bias=False, padding="same",
+                   kernel_regularizer=regularizers.l2(W_L2), kernel_initializer=KERN_INIT)(inputs)
     x_pam = Activation("relu")(x_pam)
-    x_pam = normalization(x_pam, norm, nc)
-    f_pam = Conv2D(nc // squeeze_factor, 1, kernel_regularizer=regularizers.l2(W_L2))(x_pam)
-    g_pam = Conv2D(nc // squeeze_factor, 1, kernel_regularizer=regularizers.l2(W_L2))(x_pam)
-    h_pam = Conv2D(nc, 1, kernel_regularizer=regularizers.l2(W_L2))(x_pam)
+    x_pam = normalization(x_pam, norm, num_input_channels)
+    f_pam = Conv2D(num_input_channels // squeeze_factor, 1, kernel_regularizer=regularizers.l2(W_L2))(x_pam)
+    g_pam = Conv2D(num_input_channels // squeeze_factor, 1, kernel_regularizer=regularizers.l2(W_L2))(x_pam)
+    h_pam = Conv2D(num_input_channels, 1, kernel_regularizer=regularizers.l2(W_L2))(x_pam)
+
     shape_f_pam = f_pam.get_shape().as_list()
     shape_g_pam = g_pam.get_shape().as_list()
     shape_h_pam = h_pam.get_shape().as_list()
+
     flat_f_pam = Reshape((-1, shape_f_pam[-1]))(f_pam)
     flat_g_pam = Reshape((-1, shape_g_pam[-1]))(g_pam)
     flat_h_pam = Reshape((-1, shape_h_pam[-1]))(h_pam)
+
     s_pam = Lambda(lambda x_: K.batch_dot(x_[0], Permute((2, 1))(x_[1])))([flat_g_pam, flat_f_pam])
     beta_pam = Softmax(axis=-1)(s_pam)
     o_pam = Lambda(lambda x_: K.batch_dot(x_[0], x_[1]))([beta_pam, flat_h_pam])
-    o_pam = Reshape(shape_x[1:])(o_pam)
+    o_pam = Reshape(shape[1:])(o_pam)
     o_pam = Scale()(o_pam)
     out_pam = add([o_pam, x_pam])
-    out_pam = Conv2D(nc, kernel_size=3, kernel_regularizer=regularizers.l2(W_L2),
+    out_pam = Conv2D(num_input_channels, kernel_size=3, kernel_regularizer=regularizers.l2(W_L2),
                      kernel_initializer=KERN_INIT, use_bias=False, padding="same")(out_pam)
     out_pam = Activation("relu")(out_pam)
-    out_pam = normalization(out_pam, norm, nc)
+    out_pam = normalization(out_pam, norm, num_input_channels)
 
     # channel attention module
-    x_chn = Conv2D(nc, kernel_size=3, kernel_regularizer=regularizers.l2(W_L2),
+    x_chn = Conv2D(num_input_channels, kernel_size=3, kernel_regularizer=regularizers.l2(W_L2),
                    kernel_initializer=KERN_INIT, use_bias=False, padding="same")(x)
     x_chn = Activation("relu")(x_chn)
-    x_chn = normalization(x_chn, norm, nc)
+    x_chn = normalization(x_chn, norm, num_input_channels)
     shape_x_chn = x_chn.get_shape().as_list()
     flat_f_chn = Reshape((-1, shape_x_chn[-1]))(x_chn)
     flat_g_chn = Reshape((-1, shape_x_chn[-1]))(x_chn)
     flat_h_chn = Reshape((-1, shape_x_chn[-1]))(x_chn)
     s_chn = Lambda(lambda x_: K.batch_dot(Permute((2, 1))(x_[0]), x_[1]))([flat_g_chn, flat_f_chn])
-    s_new_chn = Lambda(lambda x_: K.repeat_elements(K.max(x_, -1, keepdims=True), nc, -1))(s_chn)
+    s_new_chn = Lambda(lambda x_: K.repeat_elements(K.max(x_, -1, keepdims=True), num_input_channels, -1))(s_chn)
     s_new_chn = Lambda(lambda x_: x_[0] - x_[1])([s_new_chn, s_chn])
     beta_chn = Softmax(axis=-1)(s_new_chn)
     o_chn = Lambda(lambda x_: K.batch_dot(x_[0], Permute((2, 1))(x_[1])))([flat_h_chn, beta_chn])
-    o_chn = Reshape(shape_x[1:])(o_chn)
+    o_chn = Reshape(shape[1:])(o_chn)
     o_chn = Scale()(o_chn)
     out_chn = add([o_chn, x_chn])
-    out_chn = Conv2D(nc, kernel_size=3, kernel_regularizer=regularizers.l2(W_L2),
+    out_chn = Conv2D(num_input_channels, kernel_size=3, kernel_regularizer=regularizers.l2(W_L2),
                      kernel_initializer=KERN_INIT, use_bias=False, padding="same")(out_chn)
     out_chn = Activation("relu")(out_chn)
-    out_chn = normalization(out_chn, norm, nc)
+    out_chn = normalization(out_chn, norm, num_input_channels)
 
-    out = add([out_pam, out_chn])
-    return out
+    return add([out_pam, out_chn])
 
 
 def normalization(inp, norm='none', group=16):
@@ -216,13 +229,11 @@ def upscale_ps(input_tensor, group, use_norm=False, w_l2=W_L2, norm='none'):
 
 
 def reflect_padding_2d(x, pad=1):
-    x = Lambda(lambda x_: tf.pad(x_, [[0, 0], [pad, pad], [pad, pad], [0, 0]], mode='REFLECT'))(x)
-    return x
+    return Lambda(lambda x_: tf.pad(x_, [[0, 0], [pad, pad], [pad, pad], [0, 0]], mode='REFLECT'))(x)
 
 
 def upscale_nn(input_tensor, group, use_norm=False, w_l2=W_L2, norm='none'):
-    x = input_tensor
-    x = UpSampling2D()(x)
+    x = UpSampling2D()(input_tensor)
     x = reflect_padding_2d(x, 1)
     x = Conv2D(group, kernel_size=3, kernel_regularizer=regularizers.l2(w_l2), kernel_initializer=KERN_INIT)(x)
     x = normalization(x, norm, group) if use_norm else x
