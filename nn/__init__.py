763bca38f9e21156f8c83b649bc175c9be0da48b
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-02-23 05:33:00 +0400

    Add encoder, decoder, discriminator

--- nn/__init__.py
+++ nn/__init__.py
@@ -1,5 +1,8 @@
 from .custom_inits.icnr_initializer import icnr_keras
 from .layers.scale_layer import Scale
+from .encoder import EncoderV1 as Encoder
+from .decoder import DecoderV1 as Decoder
+from .discriminator import DiscriminatorV1 as Discriminator
 from .faceswap_model import FaceswapModel
 from .group_normalization import GroupNormalization
 from .instance_normalization import InstanceNormalization
