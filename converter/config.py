6b0557b3c747dc99e0bb251e1688d1f7c8d7577e
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-02-24 05:07:39 +0400

    Rename SegmOption to SegmentationType Add additional choices for segmentation Move ColorCorrection to color_correction.py Rename Options to ConverterConfig Rename converter/options.py to converter/config Add segmentation to config

--- converter/config.py
+++ converter/config.py
@@ -1,5 +1,7 @@
 import enum
 
+from .color_correction import ColorCorrectionType
+
 
 class TransformDirection(enum.IntEnum):
     AtoB = 0,
@@ -14,31 +16,25 @@ class ImageOutputType(enum.IntEnum):
     pass
 
 
-class SegmOption(enum.IntEnum):
+class SegmentationType(enum.IntEnum):
     ALL = 0
     EYES_ONLY = 1
-    NOSE_ONLY = 2
-    MOUTH_ONLY = 3
-    EYES_AND_MOUTH = 4
-    EYES_AND_NOSE = 5
-    MOUTH_AND_NOSE = 6
+    EYEBROWS_ONLY = 2
+    NOSE_ONLY = 3
+    MOUTH_ONLY = 4
+    EYES_AND_MOUTH = 5
+    EYES_AND_NOSE = 6
+    MOUTH_AND_NOSE = 7
     pass
 
 
-class ColorCorrection(enum.IntEnum):
-    NONE = 0
-    ADAIN = 1
-    ADAIN_XYZ = 2
-    HISTMATCH = 3
-    pass
-
-
-class Options:
+class ConverterConfig:
     def __init__(self, image_shape=None, use_smoothed_bbox=True, use_kalman_filter=True,
                  use_auto_downscaling=False, bbox_moving_avg_coef=0.65,
-                 min_face_area=35 * 35, kf_noise_coef=1e-3, color_correction=ColorCorrection.HISTMATCH,
+                 min_face_area=35 * 35, kf_noise_coef=1e-3, color_correction=ColorCorrectionType.HISTMATCH,
                  detection_threshold=0.8, roi_coverage=0.9, enhance=0.0,
-                 output_type=ImageOutputType.TRIPLE, direction=TransformDirection.AtoB):
+                 output_type=ImageOutputType.TRIPLE, direction=TransformDirection.AtoB,
+                 segmentation: SegmentationType = SegmentationType.ALL):
         self.image_shape = image_shape
         self.use_smoothed_bbox: bool = use_smoothed_bbox
         self.use_kalman_filter: bool = use_kalman_filter
@@ -46,12 +42,13 @@ class Options:
         self.bbox_moving_avg_coef: float = bbox_moving_avg_coef
         self.min_face_area = min_face_area
         self.kf_noise_coef = kf_noise_coef
-        self.color_correction: ColorCorrection = color_correction
+        self.color_correction: ColorCorrectionType = color_correction
         self.detection_threshold: float = detection_threshold
         self.roi_coverage: float = roi_coverage
         self.enhance: float = enhance
         self.output_type: ImageOutputType = output_type
         self.direction: TransformDirection = direction
+        self.segmentation_type = segmentation
 
         self._check_options()
         pass
