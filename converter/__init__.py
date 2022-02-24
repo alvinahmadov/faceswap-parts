46e4d62202bb4a55192b63700be8f6c76a67826e
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2022-02-24 06:08:29 +0400

    Update to swap parts of face

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,15 +1,29 @@
+import logging
+import os
+
 import numpy as np
 from moviepy.editor import VideoFileClip
 
-from .face_transformer import FaceTransformer
+from detector import MTCNNFaceDetector as Detector
+from utils import DummyLogger
+from .config import (
+    ConverterConfig,
+    ImageOutputType,
+    ColorCorrectionType,
+    TransformDirection,
+    SegmentationType
+)
 from .kalman_filter import KalmanFilter
 from .landmarks_alignment import (
     get_src_landmarks,
     get_tar_landmarks,
     landmarks_match_mtcnn
 )
-from .options import Options as VCOptions, ImageOutputType
+from .segmentation import FaceSegmentation
+from .transformer import FaceTransformer
 from .utils import (
+    fillzeros,
+    draw_landmarks,
     get_init_mask_map,
     get_init_comb_img,
     get_init_triple_img
@@ -17,8 +31,28 @@ from .utils import (
 
 KALMAN_FILTER_LOOP = 200
 
+logging.basicConfig(level=os.environ.get('LOG_LEVEL', 0))
+logger = logging.getLogger(__name__) \
+    if os.environ.get('ENABLE_LOGGING', False) else DummyLogger()
 
-class VideoConverter:
+DEFAULT_CONFIG = ConverterConfig(
+    image_shape=64,
+    use_smoothed_bbox=True,
+    use_kalman_filter=True,
+    use_auto_downscaling=False,
+    bbox_moving_avg_coef=0.65,
+    min_face_area=35 * 35,
+    kf_noise_coef=1e-3,
+    color_correction=ColorCorrectionType.NONE,
+    detection_threshold=0.8,
+    roi_coverage=0.9,
+    output_type=ImageOutputType.COMBINED,
+    direction=TransformDirection.BtoA,
+    segmentation=SegmentationType.ALL
+)
+
+
+class Converter:
     """
     Attributes:
         _face_transformer: FaceTransformer instance
@@ -27,47 +61,58 @@ class VideoConverter:
         _kalman_filter0, _kalman_filter1: KalmanFilter instances for smoothing bounding box
     """
 
-    def __init__(self):
+    def __init__(self, det_models_path, arch_config=None,
+                 model_path=None, config=DEFAULT_CONFIG, session=None):
+        logger.info(
+            "Initializing Converter class with parameters: %s, %s, %s, %s" % (
+                det_models_path, str(arch_config), model_path, str(session)
+            ))
         # Variables for smoothing bounding box
         self.prev_x0 = 0
         self.prev_x1 = 0
         self.prev_y0 = 0
         self.prev_y1 = 0
         self._frames = 0
-        self._face_transformer = FaceTransformer()
-        self._face_detector = None
+        self.config = config
+
+        self._face_detector = Detector(session, det_models_path)
+        self._face_transformer = FaceTransformer(arch_config, model_path) \
+            if arch_config is not None else None
+
+        self._face_segment = FaceSegmentation()
+
         self._kalman_filter0 = None
         self._kalman_filter1 = None
         pass
 
     @property
-    def gan_model(self):
+    def model(self):
         return self._face_transformer.model
 
-    @gan_model.setter
-    def gan_model(self, gm):
+    @model.setter
+    def model(self, gm):
         self._face_transformer.model = gm
         pass
 
     @property
-    def face_detector(self):
+    def detector(self):
         return self._face_detector
 
-    @face_detector.setter
-    def face_detector(self, fd):
+    @detector.setter
+    def detector(self, fd):
         self._face_detector = fd
         pass
 
-    def convert(self, input_fn, output_fn, options, duration=None):
+    def convert(self, input_fn, output_fn, duration=None, audio=False):
         """
         Parameters
         ----------
+        audio : bool
+         Save audio from original video
         input_fn : str
          Input file name
         output_fn : str
          Output file name
-        options : VCOptions
-         Options
         duration : None or tuple[int, int]
          Duration of video if specified
 
@@ -75,175 +120,233 @@ class VideoConverter:
         -------
 
         """
+        logger.info(
+            "Starting convert process for video frames from `%s` to `%s`\n and config %s." % (
+                input_fn, output_fn, str(config),
+            )
+        )
 
-        if options.use_kalman_filter:
-            self._init_kalman_filters(options.kf_noise_coef)
+        if self.config.use_kalman_filter:
+            self._init_kalman_filters(self.config.kf_noise_coef)
 
         self._frames = 0
         self.prev_x0 = self.prev_x1 = self.prev_y0 = self.prev_y1 = 0
 
-        if self.face_detector is None:
-            raise Exception(f"face detector has not been set through VideoConverter.set_face_detector() yet.")
+        if self.detector is None:
+            logger.error("Face detector has not been initialized yet.")
+            raise Exception
 
-        clip1 = VideoFileClip(input_fn)
-        if type(duration) is tuple:
-            clip = clip1.fl_image(lambda img: self.process_video(img, options)).subclip(duration[0], duration[1])
-        else:
-            clip = clip1.fl_image(lambda img: self.process_video(img, options))
-        clip.write_videofile(output_fn, audio=True)
-        clip1.reader.close()
         try:
+            clip1 = VideoFileClip(input_fn)
+            if type(duration) is tuple:
+                logger.info("Duration specified from %i to %i" % (duration[0], duration[1]))
+                clip = clip1.fl_image(lambda img: self.process_video(img, self.config)).subclip(duration[0],
+                                                                                                duration[1])
+            else:
+                clip = clip1.fl_image(lambda img: self.process_video(img, self.config))
+            clip.write_videofile(output_fn, audio=audio)
+            clip1.reader.close()
             clip1.audio.reader.close_proc()
-        except IOError as ioe:
-            print(ioe)
+        except Exception as e:
+            logger.error("Error: %s" % e)
             pass
         pass
 
-    def process_video(self, input_img, options: VCOptions):
+    def process_video(self, frame, config: ConverterConfig):
         """
         Transform detected faces in single input frame.
 
         Parameters
         ------
-        input_img : ndarray
+        frame : ndarray
          Input image from video frame
 
-        options : VCOptions
-         Options for processing
+        config : ConverterConfig
+         ConverterConfig for processing
         """
-        image = input_img
+        # image = frame
+        image_shape = self.model.image_shape \
+            if config.image_shape is None else config.image_shape
 
         # detect face using MTCNN (faces: face bbox coord, pnts: landmarks coord.)
-        faces, pnts = self.face_detector.detect_face(
-            image, minsize=20,
-            threshold=options.detection_threshold,
+        faces, pnts = self.detector.detect_face(
+            frame, minsize=20,
+            threshold=config.detection_threshold,
             factor=0.709,
-            use_auto_downscaling=options.use_auto_downscaling,
-            min_face_area=options.min_face_area
+            use_auto_downscaling=config.use_auto_downscaling,
+            min_face_area=config.min_face_area
         )
 
         # check if any face detected
         if len(faces) == 0:
-            triple_img = get_init_triple_img(input_img, no_face=True)
-            pass
+            triple_img = get_init_triple_img(frame, no_face=True)
 
-        # init. output image
+        logger.debug("Detected %i face(s) in a frame and point count %i" % (len(faces), len(pnts)))
+
         best_conf_score = 0
-        comb_img = get_init_comb_img(input_img)
-        mask_map = get_init_mask_map(image)
+        # init. output image
+        comb_img = get_init_comb_img(frame)
+        mask_map = get_init_mask_map(frame)
 
         # loop through all detected faces
         for i, (x0, y1, x1, y0, conf_score) in enumerate(faces):
+            logger.debug(
+                "Face coordinates are X(%s, %s), Y(%s, %s) with confidence score %.2f" % (
+                    x0, x1, y0, y1, conf_score
+                )
+            )
             lms = pnts[:, i:i + 1]
             # smoothe the bounding box
-            if options.use_smoothed_bbox:
+            if config.use_smoothed_bbox:
+                logger.debug("Smoothing bounding box")
                 if self._frames != 0 and conf_score >= best_conf_score:
+                    logger.debug("cond: frames != 0 and conf_score >= best_conf_score")
                     x0, x1, y0, y1 = self._get_smoothed_coord(
                         x0, x1, y0, y1,
-                        img_shape=image.shape,
-                        use_kalman_filter=options.use_kalman_filter,
-                        ratio=options.bbox_moving_avg_coef,
+                        img_shape=frame.shape,
+                        use_kalman_filter=config.use_kalman_filter,
+                        ratio=config.bbox_moving_avg_coef,
                     )
                     self._set_prev_coord(x0, x1, y0, y1)
                     best_conf_score = conf_score
                     self._frames += 1
                 elif conf_score <= best_conf_score:
+                    logger.debug("cond: conf_score <= best_conf_score")
                     self._frames += 1
                     pass
                 else:
                     if conf_score >= best_conf_score:
                         self._set_prev_coord(x0, x1, y0, y1)
                         best_conf_score = conf_score
-                    if options.use_kalman_filter:
+                    if config.use_kalman_filter:
+                        logger.debug("Using kalman filter")
                         for j in range(KALMAN_FILTER_LOOP):
                             self._kalman_filter0.predict()
                             self._kalman_filter1.predict()
+                            pass
                     self._frames += 1
                     pass
                 pass
 
             # transform face
-            try:
-                # get detected face
-                det_face_im = input_img[int(x0):int(x1), int(y0):int(y1), :]
-
-                # get src/tar landmarks
-                src_landmarks = get_src_landmarks(x0, x1, y0, y1, lms)
-                tar_landmarks = get_tar_landmarks(det_face_im)
-
-                # align detected face
-                aligned_det_face_im = landmarks_match_mtcnn(det_face_im, src_landmarks, tar_landmarks)
-
-                # face transform
-                r_im, r_rgb, r_a = self._face_transformer.transform(
-                    aligned_det_face_im,
-                    direction=options.direction,
-                    roi_coverage=options.roi_coverage,
-                    color_correction=options.color_correction,
-                    image_shape=options.image_shape
-                )
-
-                # reverse alignment
-                rev_aligned_det_face_im = landmarks_match_mtcnn(r_im, tar_landmarks, src_landmarks)
-                rev_aligned_det_face_im_rgb = landmarks_match_mtcnn(r_rgb, tar_landmarks, src_landmarks)
-                rev_aligned_mask = landmarks_match_mtcnn(r_a, tar_landmarks, src_landmarks)
-
-                # merge source face and transformed face
-                result = np.zeros_like(det_face_im)
-                result = rev_aligned_mask / 255 * rev_aligned_det_face_im_rgb + (
-                        1 - rev_aligned_mask / 255) * det_face_im
-                result_a = rev_aligned_mask
-            except:
-                # catch exceptions for landmarks alignment errors (if any)
-                print(f"Face alignment error occurs at frame {self._frames}.")
-                # get detected face
-                det_face_im = input_img[int(x0):int(x1), int(y0):int(y1), :]
-
-                result, _, result_a = self._face_transformer.transform(
-                    det_face_im,
-                    direction=options.direction,
-                    roi_coverage=options.roi_coverage,
-                    color_correction=options.color_correction,
-                    image_shape=options.image_shape
-                )
-                pass
-
-            comb_img[
-             int(x0):int(x1),
-             input_img.shape[1] + int(y0):input_img.shape[1] + int(y1),
-             :] = result
-
-            # Enhance output
-            if options.enhance != 0:
-                comb_img = -1 * options.enhance * get_init_comb_img(input_img) + (1 + options.enhance) * comb_img
-                comb_img = np.clip(comb_img, 0, 255)
-                pass
-
-            if conf_score >= best_conf_score:
-                mask_map[int(x0):int(x1), int(y0):int(y1), :] = result_a
-                mask_map = np.clip(mask_map + .15 * input_img, 0, 255)
-                # Possible bug: when small faces are detected before the most confident face,
-                #               the mask_map will show brighter input_img
-                pass
-            else:
-                mask_map[int(x0):int(x1), int(y0):int(y1), :] += result_a
-                mask_map = np.clip(mask_map, 0, 255)
-                pass
-
-            triple_img = get_init_triple_img(input_img)
-            triple_img[:, :input_img.shape[1] * 2, :] = comb_img
-            triple_img[:, input_img.shape[1] * 2:, :] = mask_map
+            comb_img, triple_img = self._transform(
+                frame, image_shape, face=(x0, x1, y0, y1, conf_score),
+                config=config, comb_img=comb_img,
+                mask_map=mask_map, lms=lms, face_idx=i + 1
+            )
             pass
 
-        if options.output_type == ImageOutputType.SINGLE:
-            return comb_img[:, input_img.shape[1]:, :]
-        elif options.output_type == ImageOutputType.COMBINED:
+        if config.output_type == ImageOutputType.SINGLE:
+            return comb_img[:, frame.shape[1]:, :]
+        elif config.output_type == ImageOutputType.COMBINED:
             return comb_img
-        elif options.output_type == ImageOutputType.TRIPLE:
+        elif config.output_type == ImageOutputType.TRIPLE:
             return triple_img
         pass
 
+    def _transform(self, frame, image_shape, face,
+                   config, comb_img, mask_map, lms, face_idx=0):
+        """
+        Here goes actual transformation for source and target faces
+        Parameters
+        ----------
+        frame : ndarray
+         Frame from video with target face
+        image_shape
+        face
+        config
+        comb_img
+        mask_map
+        lms
+        face_idx
+
+        Returns
+        -------
+
+        """
+        logger.info("Starting face transformation for face %i" % face_idx)
+        # init. output image
+        best_conf_score = 0.0
+        x0, x1, y0, y1, conf_score = face
+        # get detected face
+        detected_face_img = frame[int(x0):int(x1), int(y0):int(y1), :]
+
+        try:
+            # get src/tar landmarks
+            src_landmarks = get_src_landmarks(x0, y0, lms, face_idx - 1)
+            tar_landmarks = get_tar_landmarks(detected_face_img)
+
+            # align detected face
+            aligned_det_face_im = landmarks_match_mtcnn(detected_face_img, src_landmarks, tar_landmarks)
+
+            # face transform
+            r_im, r_rgb, r_alpha = self._face_transformer.transform(aligned_det_face_im,
+                                                                    config.direction,
+                                                                    config.roi_coverage,
+                                                                    config.color_correction,
+                                                                    image_shape)
+
+            # reverse alignment
+            rev_aligned_det_face_im_rgb = landmarks_match_mtcnn(r_rgb, tar_landmarks, src_landmarks)
+            rev_aligned_mask = landmarks_match_mtcnn(r_alpha, tar_landmarks, src_landmarks)
+
+            seg_im_rgb = self._face_segment.get_segments(
+                detected_face_img, rev_aligned_det_face_im_rgb, config.segmentation_type
+            )
+
+            # merge source face and transformed face
+            # TODO(alvin): Maybe needed to reverse masking for aligned_im_a and aligned_im_b
+            #  (which does no swap).
+            aligned_im_a: np.ndarray = (1 - rev_aligned_mask / 255) * seg_im_rgb
+            aligned_im_b: np.ndarray = (rev_aligned_mask / 255) * detected_face_img
+
+            # We merge original image and swapped image parts
+            result = aligned_im_a + aligned_im_b
+            result_a = rev_aligned_mask
+
+        except Exception as ex:
+            # catch exceptions for landmarks alignment errors (if any)
+            logger.error(f"Face alignment error occurred at frame {self._frames}. "
+                         f"Transforming without alignment."
+                         f"\nError {ex}",
+                         exc_info=True)
+
+            result, _, result_a = self._face_transformer.transform(
+                detected_face_img,
+                direction=config.direction,
+                roi_coverage=config.roi_coverage,
+                color_correction=config.color_correction,
+                image_shape=image_shape
+            )
+            pass
+
+        # create image with original and swapped frames
+        comb_img[int(x0):int(x1), frame.shape[1] + int(y0):frame.shape[1] + int(y1), :] = result
+
+        # Enhance output
+        if config.enhance != 0:
+            comb_img = -1 * config.enhance * get_init_comb_img(frame) + (1 + config.enhance) * comb_img
+            comb_img = np.clip(comb_img, 0, 255)
+            pass
+
+        if conf_score >= best_conf_score:
+            mask_map[int(x0):int(x1), int(y0):int(y1), :] = result_a
+            mask_map = np.clip(mask_map + .15 * frame, 0, 255)
+            pass
+        else:
+            mask_map[int(x0):int(x1), int(y0):int(y1), :] += result_a
+            mask_map = np.clip(mask_map, 0, 255)
+            pass
+
+        # create frame with original, swapped and mask frame
+        triple_img = get_init_triple_img(frame)
+        triple_img[:, :frame.shape[1] * 2, :] = comb_img
+        triple_img[:, frame.shape[1] * 2:, :] = mask_map
+
+        return comb_img, triple_img
+
     def _get_smoothed_coord(self, x0, x1, y0, y1, img_shape, use_kalman_filter=True, ratio=0.65):
+        logger.debug(f"Smoothening face coordinates with ratio: {ratio}")
         if not use_kalman_filter:
             x0 = int(ratio * self.prev_x0 + (1 - ratio) * x0)
             x1 = int(ratio * self.prev_x1 + (1 - ratio) * x1)
@@ -265,6 +368,9 @@ class VideoConverter:
         return x0, x1, y0, y1
 
     def _set_prev_coord(self, x0, x1, y0, y1):
+        logger.debug("Setting previous coordinates X(%s, %s), Y(%s, %s)" % (
+            x0, x1, y0, y1
+        ))
         self.prev_x0 = x0
         self.prev_x1 = x1
         self.prev_y1 = y1
